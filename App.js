26f229918abd3ad9be0b8c7619f09aa36a107ddc
John Roger <johnrogerargarin@gmail.com>
2022-12-04 18:28:20 +0800

    water

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View, ImageBackground} from 'react-native';
 import Navigator from './app/routes/welcome_loginStack';
 
 export default function App() {
-  const image = { uri: "../assets/logo3.png" };
+  
   
   return (
-    <ImageBackground source={image} style={styles.image}>
+  
    <Navigator />
-   </ImageBackground>
+   
   );
 }
 
